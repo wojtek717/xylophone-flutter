689b7acd72999c489ddb9325c2fb8361aaf9c5f6
wojtek717 <wojciechkonury@gmail.com>
2019-10-07 13:25:42 +0200

    First UI, playing sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumb){
+    final player = AudioCache();
+    player.play('note$soundNumb.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text('b1'),
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text('b2'),
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text('b3'),
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text('b4'),
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text('b5'),
+                color: Colors.teal,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text('b6'),
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text('b7'),
+                color: Colors.purple,
+              ),
+            ],
+          ),
         ),
       ),
     );
